48d909cdffa5ef80684312d5801e48e3d16a6059
Braxai <ianoc2276@gmail.com>
2025-04-19 21:56:38 -0700

    Added typing animations in the intro section, hover animations for all text paragraphs to make them more readable.
    Intro description indicates employment status.

--- js/index.js
+++ js/index.js
@@ -10,3 +10,38 @@ navLinks.forEach(link => {
         document.body.classList.remove('nav-open');
     })
 })
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("DOM loaded, starting animation");
+    const name = "O'Connell";
+    const target = document.getElementById("typing-name");
+    const cursor = document.getElementById("cursor");
+    let index = 0;
+
+    function typeNextChar() {
+        if (index < name.length) {
+            //target.textContent += name.charAt(index);
+            cursor.insertAdjacentText('beforebegin', name.charAt(index));
+            index++;
+            setTimeout(typeNextChar, 200);
+        } else {
+            cursor.remove();
+            startRoleAnimation();
+        }
+    }
+
+    function startRoleAnimation() {
+        var typed = new Typed(".auto-type", {
+            strings: ["Student", "Programmer", "Manager"],
+            typeSpeed: 50,
+            backSpeed: 50,
+            startDelay: 500,
+            backDelay: 1000,
+            loop: true,
+        });
+    }
+
+    // Start the name animation immediately
+    //typeNextChar();
+    setTimeout(typeNextChar, 250);
+});
