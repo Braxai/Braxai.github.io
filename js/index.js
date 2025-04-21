7d718bf6a6b93517a5b28114abbcee699898043a
Braxai <ianoc2276@gmail.com>
2025-04-20 20:19:25 -0700

    Added waved section borders. Code for an animated blob is commented out, need to mess around with the positioning of the blob.

--- js/index.js
+++ js/index.js
@@ -53,3 +53,12 @@ const observer = new IntersectionObserver((entries) => {
   }, { threshold: 0.1 });
   
 document.querySelectorAll('.animate').forEach(el => observer.observe(el));
+
+const tween = KUTE.fromTo(
+    '#blob1',
+    { path: '#blob1' },
+    { path: '#blob2' },
+    { repeat: 999, duration: 3000, yoyo: true }
+)
+
+tween.start();
